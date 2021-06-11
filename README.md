18d1f240385796caa0cc00ea992b808fbd060c1d
leresi <kevinleparwa@gmail.com>
2021-06-11 09:49:30 +0300

    added files

---
+++ README.md
@@ -0,0 +1,27 @@
+## CALCULATE USER DAY OF WEEK
+#### Kevin Leresi Leparwa
+## **Description**
+#### this project calculate the day of the week a user is born. Based on some logics we i display to the user their Kwasi name after they input their gender  
+
+## TECHNOLOGIES USED
+
+|*NO*|Name| 
+|---------|------------|
+| #1 | HTML | 
+| #2 | CSS | 
+| #3 | javaScript | 
+
+
+## **Contributors**
+#### The website project was fully done by: **Kevin Leresi Leparwa**
+## **Setup/Installation**
+#### No installation required.You only need access with a connected device to the internet with working browser(Chrome browser is advisable)
+##  **Known Bugs**
+#### There are no Bugs on this file but should you experience any difficulties in accessing or getting the information you need, or come across anything kindly share with me via my email [email](kevinleparwa@gmail.com)
+## My gh-pages link
+[gh-page](https://leparwa.github.io/myPortFolio/)
+## My contact details
+you can find me on twitter @kevin_leresi
+## Licence
+Licensed under the *[MIT LICENSE](LICENSE.txt)*
+Copyright (c) 2021 *Leresi Kevin Leparwa*
---
+++ index.js
@@ -0,0 +1,120 @@
+var cc= parseInt(prompt("enter the century") )
+var yy = parseInt(prompt("enter the year"))
+var mm = parseInt(prompt("enter the month"))
+var dd = parseInt(prompt("enter the day"))
+var maleAkanaNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "kofi", "Kwame"]
+var femaleAkanaNames=["akosua", "Adwoa", "Abenaa", "akua", "yaa", "afua", "ama"]
+function calculate(){
+ 
+  if(cc  >=  19 && cc <= 20)
+  {
+  let yourDayOfWeek = Math.round(
+    (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )%7
+  )
+  console.log(typeof(yourDayOfWeek))
+  console.log(yourDayOfWeek)
+  // return Math.round(yourDayOfWeek)
+  if(yourDayOfWeek === 1){
+    console.log("value is 1")
+    let gender = prompt("enter the gender");
+    if (gender === 'male'){
+          console.log("your akana name is" +maleAkanaNames[0])
+    }
+    else if(gender === "female"){
+    console.log("your akana name is" +femaleAkanaNames[1])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+  else if(yourDayOfWeek === 2){
+    let gender = prompt("enter the gender")
+    if( gender ==="male"){
+   console.log("your akana name is" +maleAkanaNames[1])
+    }
+     else if( gender == "female"){
+    console.log("your akana name is" +femaleAkanaNames[1])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+   
+  }
+ else if(yourDayOfWeek === 3){
+    console.log("value is 3")
+    let gender = prompt("enter the gender")
+    if(gender =="male"){
+          console.log("your akana name is" +maleAkanaNames[2])
+    }
+    else if( gender == "female"){
+    console.log("your akana name is " +femaleAkanaNames[2])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+  else if(yourDayOfWeek === 4){
+     console.log("value is 4")
+    let gender = prompt("enter the gender")
+    if(gender =="male"){
+          console.log("your akana name is" +maleAkanaNames[3])
+    }
+    else if( gender == "female"){
+    console.log("your akana name is " +femaleAkanaNames[3])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+  else if(yourDayOfWeek === 5){
+     console.log("value is 5")
+    let gender = prompt("enter the gender")
+    if(gender == "male"){
+          console.log("your akana name is" +maleAkanaNames[4])
+    }
+    else if( gender == "female"){
+    console.log("your akana name is" +femaleAkanaNames[4])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+  else if(yourDayOfWeek === 6){
+     console.log("value is 6")
+    let gender = prompt("enter the gender")
+    if(gender == "male"){
+          console.log("your akana name is" +maleAkanaNames[5])
+    }
+   else if( gender == "female"){
+    console.log("your akana name is" +femaleAkanaNames[5])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+ else if(yourDayOfWeek === 7){
+    console.log("value is 7")
+    let gender = prompt("enter the gender")
+    if(gender == "male"){
+          console.log("your akana name is" +maleAkanaNames[6])
+    }
+    else if( gender == "female"){
+    console.log("your akana name is " +femaleAkanaNames[6])
+    }
+    else{
+      console.log("No Kwasi Name for you")
+    }
+  }
+else{
+  console.log("wrong calculation")
+}
+    // console.log("you proceed")
+  }
+ 
+  else{
+    console.log("input a valid century")
+  }
+
+
+}
+calculate()
